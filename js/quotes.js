e7da0fab428c9ee8391f43df320aa57052c5fbbb
bona1122 <vnclfjqm12@gmail.com>
2024-02-04 21:05:59 +0900

    login, clock, quotes and background image

---
+++ js/quotes.js
@@ -0,0 +1,38 @@
+const quotes = [
+  {
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+  },
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  },
+  {
+    quote:
+      "Your time is limited, so don't waste it living someone else's life.",
+    author: "Steve Jobs",
+  },
+  {
+    quote:
+      "If life were predictable it would cease to be life, and be without flavor.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  },
+  {
+    quote:
+      "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+  },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysOuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysOuote.quote;
+author.innerText = todaysOuote.author;
